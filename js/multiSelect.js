89089c9448fd5ed8eea90e31400dd345144bcd85
craigcosmo <craigcosmo@gmail.com>
2015-08-25 21:53:06 +0700

    update
    added index attribute
    make it work when sibling element is not select

--- js/multiSelect.js
+++ js/multiSelect.js
@@ -1,6 +1,15 @@
 (function($){
-	$.fn.multiSelect = function(delimiter){
-		delimiter = (delimiter == undefined) ? '-' : delimiter;
+	$.fn.multiSelect = function(options){
+
+
+
+		var defaults = {
+			callback: function(){},
+			delimiter: '-'
+		},
+		o = $.extend({},defaults, options);
+
+
 		var $this = $(this);
 		var selects = $this.find('select');
 		var arr = [];
@@ -11,42 +20,48 @@
 				arr[index][this.value] = this.innerHTML;
 			});
 		});
+
+
 		selects.each(function(index, el){
 			var this_options = $(el).find('option');
 			var this_options_selected = $(el).find('option:selected');
 			var val = this_options_selected.val();
 			
 			if(index< selects.length){
-				var reg = new RegExp('^'+val+'(?='+delimiter+')');
+				var reg = new RegExp('^'+val+'(?='+o.delimiter+')');
 				selects.eq(index+1).find('option').each(function(){
 					if(!this.value.match(reg)){
-						$(this).remove();
+						this.outerHTML = "";
 					}
 				});
 			}
 		})
-		selects.on('click change', 'option', function(e){
-			var next_sib_index = $(e.target).parent().index() + 1;
-			console.log(next_sib_index);
-			// alert(next_sib_index);
+		$this.on('change', 'select', function(e){
+			// console.log($(e.target).attr('ind'));
+			// console.log($(this).attr('ind'));
+
+			var next_sib_index = parseInt($(this).attr('ind')) + 1;
+			//  why use attr ind becuase can not use index() if the siblings include different element rather than select.
 			for(n=next_sib_index; n<selects.length; n++){
-				console.log(next_sib_index);
 				selects.eq(n).each(function(){
 					this.length=0;
 				});
 			}
+			var selected_option = $(e.target).val();
 			if(next_sib_index < selects.length){
 				// alert(selects.length);
-				var regex = new RegExp('^'+$(e.target).val()+'(?='+delimiter+')');// match x if only followed by y
-				console.log(regex);
+				// match value that follow by '-'
+				var regex = new RegExp('^'+$(e.target).val()+'(?='+o.delimiter+')');
+				
+
 				$.each(arr[next_sib_index], function(key,val){
-					// console.log('key: '+key +' val: '+val);	
 					if(key.match(regex)){
-						// console.log(selects.eq());
+
 						selects.eq(next_sib_index).append('<option value="'+ key +'">'+ val +'</option>');
 					}
 				});
 			}
+			o.callback.call(this);
 		});
 	};
 })(jQuery);
